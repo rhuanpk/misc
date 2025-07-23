4a8f24b8651236e5b12de323efc62355186d4a1d
rhuanpk <dev@rhuanpk.net>
2025-07-22 22:16:45 -0300

    feat: upgrade getopts example

--- getopts-example/getopts_example.sh
+++ getopts-example/getopts_example.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# use case for testing: `./getopts_example.sh -azb`.
-# - the ":" in the beginning of the string options is for error suppression;
-# - the ":" after an option makes it require an argument;
+# when not in silent mode
+# 	- error messages are printed
+# 	- invalid option
+# 		- unset OPTARG
+# 		- flag=?
+# 	- required argument not found
+# 		- unset OPTARG
+# 		- flag=?
+#
+# ":" in the beginning of the string options: makes silent mode
+# 	- no error messages are printed
+# 	- invalid option
+# 		- OPTARG=<flag>
+# 		- flag=?
+# 	- required argument not found
+# 		- OPTARG=<flag>
+# 		- flag=:
+#
+# ":" after an option makes it require an argument
 
-while getopts ':ab:' option; do
-	case $option in
-		a) echo "option: $option - argument: ${OPTARG}";;
-		b) echo "option: $option - argument: ${OPTARG}";;
-		:) echo "option: $option - argument: ${OPTARG} → missing argument!";;
-		?) echo "option: $option - argument: ${OPTARG} → invalid option!";;
+while getopts 'ab:' flag; do
+	case $flag in
+		a) echo "flag: ${flag@Q} | arg: ${OPTARG@Q}";;
+		b) echo "flag: ${flag@Q} | arg: ${OPTARG@Q}";;
+		:) echo "flag: ${flag@Q} | arg: ${OPTARG@Q} (missing arg)";;
+		?) echo "flag: ${flag@Q} | arg: ${OPTARG@Q} (invalid flag)";;
 	esac
 done
 
